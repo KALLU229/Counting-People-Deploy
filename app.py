92d98378e7edbcbba1b2a3e0165d4baf2441ac90
KALLU229 <kanishkashaw462@gmail.com>
2026-04-13 01:45:17 +0530

    fix libGL error

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from views.login import login_page
 from views.register import register_page
 from views.dashboard import dashboard
 # from views.detection import detection_page
-from views.live_camera import live_camera_page
+# from views.live_camera import live_camera_page
 from views.admin_panel import admin_panel
 from auth.jwt_utils import verify_token
 
@@ -45,9 +45,9 @@ else:
 
     if page == "Dashboard":
         dashboard()
-    elif page == "Detection":
-        detection_page()
-    elif page == "Live Camera":
-        live_camera_page()
+    # elif page == "Detection":
+    #     detection_page()
+    # elif page == "Live Camera":
+    #     live_camera_page()
     elif page == "Admin Panel":
         admin_panel()
